7d98d074fa0aaba4fefed7d82bfd445e783318ed
janghyunjoon <135941433+janghyunjoon@users.noreply.github.com>
2025-08-04 12:19:12 +0900

    .

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('#container')
+const title = document.querySelector('.title')
+
+const playBtn = document.querySelector('#play')
+const stopBtn = document.querySelector('#stop')
+let play  = setInterval(bgChange,1000)
+
+function getrandomColor(){
+    let hex ='#'+Math.random().toString(16).slice(2,8)
+
+    return hex
+}
+
+function bgChange(){
+let randomColor = getrandomColor()
+
+container.style.backgroundColor=randomColor
+title.textContent=randomColor
+
+}
+
+
+stopBtn.addEventListener('click',()=>{
+    clearInterval(play)
+    play=null
+})
+playBtn.addEventListener('click',()=>{
+    if(play){
+        let play  = setInterval(bgChange,2000)
+    }
+})
